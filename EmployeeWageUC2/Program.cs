413e58bb152cde30b24fb953fbede53d96bfc73a
naveen962 <mrn28897@gmail.com>
2021-09-27 22:55:03 -0700

    program to calculate Daily Employee wage is added

---
+++ EmployeeWageUC2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EmployeeCheck
+{
+    class Program
+    {
+        public void DailyWages()
+        {
+            int wage_per_hour = 20;
+            int fulltime = 8;
+            //Random object created
+            Random ran = new Random();
+            int EmpCheck = ran.Next(0, 2);
+
+            //condition
+            if (EmpCheck == 1)
+            {
+                Console.WriteLine("EMPLOYEE IS PRESENT");
+                int DailyWages = wage_per_hour * fulltime;
+                Console.WriteLine("Daily wage = " + DailyWages);
+            }
+            else
+            {
+                Console.WriteLine("EMPLOYEE IS ABSENT");
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            //object created
+            Program p = new Program();
+            p.DailyWages();
+        }
+    }
+}
+
