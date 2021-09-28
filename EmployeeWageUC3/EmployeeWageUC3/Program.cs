52ac5a81060aadb69be03dcc0f74ce6571ed67f1
naveen962 <mrn28897@gmail.com>
2021-09-27 22:58:03 -0700

    add art time employee and wage program is added

---
+++ EmployeeWageUC3/EmployeeWageUC3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWages
+{
+    class UseCase3
+    {
+        public  void AddPartimeEmp()
+
+        {
+            //constants
+            int PART_TIME = 1;
+            int FULL_TIME = 2;
+            int EMPWAGE_PER_HOUR = 20;
+
+            //variables
+            int EmpHours = 0;
+
+            Random ran = new Random();
+            int Empcheck = ran.Next(0, 3);
+            if (Empcheck == PART_TIME)
+            {
+                EmpHours = 4;
+            }
+            else if (Empcheck == FULL_TIME)
+            {
+                EmpHours = 8;
+            }
+            else
+            {
+                EmpHours = 0;
+                Console.WriteLine("ABSENT");
+
+            }
+            int Empwage = EmpHours * EMPWAGE_PER_HOUR;
+            Console.WriteLine("EMPLOYEE WAGE =" + Empwage);
+        }
+
+
+        static void Main(string[] args)
+        {
+            UseCase3 uc = new UseCase3();
+            uc.AddPartimeEmp();
+            
+          
+        }
+    }
+}
+
+
